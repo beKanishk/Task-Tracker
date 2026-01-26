631c0101c998c673933f13646825fcae3e793752
Iamhacktree <akanishk76@gmail.com>
2026-01-26 13:46:45 +0530

    me api added

--- src/main/java/com/task/tracker/controller/UserController.java
+++ src/main/java/com/task/tracker/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.task.tracker.controller;
 
+import com.task.tracker.authentication.service.AuthService;
 import com.task.tracker.dto.UserRequestDTO;
 import com.task.tracker.dto.UserResponseDTO;
+import com.task.tracker.model.User;
 import com.task.tracker.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +15,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthService authService;
+
     @PostMapping
     public UserResponseDTO createUser(@RequestBody UserRequestDTO dto) {
         return userService.createUser(dto);
+    }
+    @GetMapping("/me")
+    public UserResponseDTO me(@RequestHeader("Authorization") String token) {
+        return authService.getUserFromToken(token);
     }
 }
